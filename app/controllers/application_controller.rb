bb7ff485f6cf4f4a0d1c1776f74600ab0b0d207b
Lamec Fletez <lfletez@email.arizona.edu>
2017-08-29 19:42:36 -0700

    CSRF null session for APIs. Enabled 3rd party controllers in Gems and correct MIME type. Change params to snake_case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # using null_session for APIs. Also stops CSRF attacks and creates an exception
+  protect_from_forgery with: :null_session
+
+  respond_to :json
+
+  before_action :underscore_params!
+
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
+  end
+
 end
